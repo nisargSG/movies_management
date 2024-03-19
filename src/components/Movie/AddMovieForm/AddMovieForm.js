cfc5857a1c13fef618ac9c88a510786f1136879d
nisargSG <nisargsg.tops@gmail.com>
2024-03-19 09:10:54 +0530

    -prevent form submission

--- src/components/Movie/AddMovieForm/AddMovieForm.js
+++ src/components/Movie/AddMovieForm/AddMovieForm.js
@@ -1,36 +1,40 @@
 import './AddMovieForm.css'
-import {callAPI} from '../../../util'
+import { callAPI } from '../../../util'
 import { useState } from 'react'
 
 
 export default function AddMovieForm(props) {
 
-    const [title,setTitle]= useState("")
-    const [views,setViews]= useState("")
-    const [image,setImage]= useState("")
+    const [title, setTitle] = useState("")
+    const [views, setViews] = useState("")
+    const [image, setImage] = useState("")
 
-    const [taskStatus,setTaskStatus] = useState()
+    const [taskStatus, setTaskStatus] = useState()
 
 
-    const addDetailsToServer=()=>{
+    const addDetailsToServer = (e) => {
 
-        const newMovieData={
-            title:title,
-            views:views,
-            image:image
+        e.preventDefault()
+
+        const newMovieData = {
+            title: title,
+            views: views,
+            image: image
         }
-        callAPI({method:"POST",requestBody:newMovieData,callBackFunction:function(data){
-            if(data){
-                setTaskStatus(true)
-                setTitle("")
-                setViews("")
-                setImage("")
-            }
-            else{
-                setTaskStatus(false)
-            }
+        callAPI({
+            method: "POST", requestBody: newMovieData, callBackFunction: function (data) {
+                if (data) {
+                    setTaskStatus(true)
+                    setTitle("")
+                    setViews("")
+                    setImage("")
+                }
+                else {
+                    setTaskStatus(false)
+                }
 
-        }})
+            }
+        })
     }
 
 
@@ -38,23 +42,23 @@ export default function AddMovieForm(props) {
 
         <div className="container m-4 p-4 container_add">
             <h1>Add new Movie</h1>
+            <form>
+                <div class="mb-3">
+                    <label for="etxMovieTitle" className="form-label">Movie Title</label>
+                    <input type="text" value={title} onChange={(e) => { setTitle(e.target.value) }} className="form-control" id="etxMovieTitle" placeholder="Enter Movie Title" required />
+                </div>
+                <div class="mb-3">
+                    <label for="etxMovieViews" className="form-label">Movie Views</label>
+                    <input type="text" value={views} onChange={(e) => { setViews(e.target.value) }} className="form-control" id="etxMovieViews" placeholder="Enter Movie Views" required/>
+                </div>
+                <div class="mb-3">
+                    <label for="etxMovieImage" className="form-label">Movie Image</label>
+                    <input type="text" value={image} onChange={(e) => { setImage(e.target.value) }} className="form-control" id="etxMovieImage" placeholder="Enter Movie Image URL" required/>
+                </div>
 
-            <div class="mb-3">
-                <label for="etxMovieTitle" className="form-label">Movie Title</label>
-                <input type="text" value={title} onChange={(e) => { setTitle(e.target.value) }} className="form-control" id="etxMovieTitle" placeholder="Enter Movie Title" />
-            </div>
-            <div class="mb-3">
-                <label for="etxMovieViews" className="form-label">Movie Views</label>
-                <input type="text" value={views} onChange={(e) => { setViews(e.target.value) }} className="form-control" id="etxMovieViews" placeholder="Enter Movie Views" />
-            </div>
-            <div class="mb-3">
-                <label for="etxMovieImage" className="form-label">Movie Image</label>
-                <input type="text" value={image} onChange={(e) => { setImage(e.target.value) }} className="form-control" id="etxMovieImage" placeholder="Enter Movie Image URL" />
-            </div>
-
-            <button className="btn btn-danger m-2" onClick={addDetailsToServer}>Add</button>
-            <button className="btn btn-warning m-2" onClick={() => props.setCurrentState({ mode: "LIST" })}>Back</button>
-
+                <button type='submit' className="btn btn-danger m-2" onClick={addDetailsToServer}>Add</button>
+                <button className="btn btn-warning m-2" onClick={() => props.setCurrentState({ mode: "LIST" })}>Back</button>
+            </form>
             {taskStatus == false && <div className="alert alert-warning" role="alert">
                 Failed To Add
             </div>}
